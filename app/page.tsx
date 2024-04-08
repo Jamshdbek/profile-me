a1729af2ae326b3e57e0ecb20677454e8a74bcb1
jamshidbekd664@gmail.com <jamshidbekd664@gmail.com>
2024-04-08 19:45:22 +0500

    fix changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      profile page
+    <main className="flex pt-24 mx-[14vw] justify-center">
+      <div className="bg-blue w-full">
+        <h1>Welcome to my page IBRO</h1>
+      </div>
+      <br />
+      <svg viewBox="0 0 340 333">
+        <path
+          className="path"
+          fill="dark"
+          stroke="blue"
+          stroke-width="4"
+          d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
+s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
+C46.039,146.545,53.039,128.545,66.039,133.545z"
+        />
+      </svg>
     </main>
   );
 }
