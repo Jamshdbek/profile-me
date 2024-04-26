1597e9099e69210da99ab9ce4d16233e481ac5e1
jamshidbekd664@gmail.com <jamshidbekd664@gmail.com>
2024-04-26 16:24:42 +0500

    [push changes]

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex pt-24 mx-[14vw] justify-center">
-      <div className="bg-blue w-full">
-        <h1>Jamshid Ibroximov</h1>
+    <main className="flex pt-24 mx-[14vw] justify-center ">
+      <div className="bg-blue w-full ">
+        <h1 className=" text-5xl">This Website update progress...  </h1>
+
       </div>
       <br />
-      {/* <svg viewBox="0 0 340 333">
+      <svg viewBox="0 0 340 333">
         <path
           className="path"
           fill="dark"
@@ -15,7 +16,7 @@ export default function Home() {
 s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
 C46.039,146.545,53.039,128.545,66.039,133.545z"
         />
-      </svg> */}
+      </svg>
     </main>
   );
 }
