2d7a14b2c56abd4108dfc8ec7cca4ebe557d4964
jamshidbekd664@gmail.com <jamshidbekd664@gmail.com>
2024-05-08 14:13:15 +0500

    push changes

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function index() {
-  return <div className="w-full"></div>;
+  return <div className="w-full border border-t mt-20  border-gray-500 p-20">Jamshid Ibrokhimov</div>;
 }
 
 export default index;
--- app/pages/projects/index.tsx
+++ app/pages/projects/index.tsx
@@ -1,17 +1,55 @@
+import Image, { StaticImageData } from "next/image";
+import paybek from "@/assets/image/panel.png";
+import myUHL from "@/assets/image/my_uhl.jpg";
+interface ItemType {
+  name: string;
+  link: string;
+  description: string;
+  img: StaticImageData;
+}
 function index() {
+  const projects: ItemType[] = [
+    {
+      name: "Paybek",
+      description: "SRM System Panel for web ",
+      link: "",
+      img: paybek,
+    },
+    {
+      name: "My Uhl",
+      description: "SRM System Panel for web",
+      link: "https://www.my-uhl.com/",
+      img: myUHL,
+    },
+    {
+      name: "Smart Factory",
+      description: "SRM System Panel for web",
+      link: "https://www.my-uhl.com/",
+      img: paybek,
+    },
+  ];
   return (
     <div className="flex justify-between mt-[30vh]">
       {" "}
       <h1 className="teg_text">Projects</h1>
-      <div className=" flex gap-1 border-collapse border-white max-w-[75vh]">
-        <div className=" min-w-[20vh] w-full text-start ">
-          <h2 className=" text-lg">Paybek</h2>
-          <p className=" text-sm">SRM System Panel for web</p>
-        </div>
-        <div className=" min-w-[20vh] w-full text-start ">
-          <h2 className=" text-lg">Lamiun</h2>
-          <p className=" text-sm">Learning app</p>
-        </div>
+      <div className="  gap-1 border-collapse border-white max-w-[75vh] block">
+        {" "}
+        {projects.map((item: ItemType) => (
+          <div
+            key={item.name}
+            className=" mt-2 max-h-[20vh] max-w-[70vh] min-w-[80vh] flex border rounded-[10px] gap-4 pl-3 pr-3 pt-3 pb-3  border-collapse border-white"
+          >
+            <Image
+              src={item.img}
+              className=" rounded-[10px] max-w-[30vh]"
+              alt="404"
+            />
+            <div>
+              <h2 className=" text-xl">{item.name}</h2>
+              <p className=" text-sm">{item.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
