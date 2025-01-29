6be68fbf7765584a66f7a00bc4b72e529ce77d15
Jamshid <jamshidbekd664@gmail.com>
2025-01-29 12:57:31 +0500

    feat: google analice qo'shildi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import Next.js Script
 import "./globals.css";
 import { Footer } from "@/app/components/footer";
 import { ThemeProvider } from "./provider";
@@ -8,8 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jamshid Portfolio",
-  description: "personal portfolio website  Jamshid Ibrokhimov",
-  authors: [{ name: "jamshidbek ibroximov" }],
+  description: "Personal portfolio website of Jamshid Ibrokhimov",
+  authors: [{ name: "Jamshidbek Ibrokhimov" }],
   keywords:
     "jamshid ibroximov ibro uz web developer frontend react developer jsx react developer ibro.uz ibro ",
 };
@@ -21,6 +22,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NPJYTM9SF8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NPJYTM9SF8', { page_path: window.location.pathname });
+        `}
+      </Script>
+
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
